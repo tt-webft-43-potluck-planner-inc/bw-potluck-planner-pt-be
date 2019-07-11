a28473e666a18ae4b333c4d978532b52cc70f712
Christopher Harrison <cnharrison@gmail.com>
2019-07-10 21:51:38 -0400

    create dummy users model and test file

---
+++ data/models/usersModel.js
@@ -0,0 +1,29 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  insert,
+  update,
+  remove,
+  getAll,
+  findById
+};
+
+async function insert(user) {
+  return null;
+}
+
+async function update(id, newUser) {
+  return null;
+}
+
+async function remove(id) {
+  return null;
+}
+
+async function getAll() {
+  return null;
+}
+
+async function findByID() {
+  return null;
+}
---
+++ data/models/usersModel.spec.js
@@ -0,0 +1,2 @@
+const db = require("../data/dbConfig.js");
+const Users = require("./usersModel.js");
