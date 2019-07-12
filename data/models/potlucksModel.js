1541a845459784e13a1bf67bc594c3b65977b72c
Derek Jones <derek@derekjones.io>
2019-07-11 22:19:57 -0400

    Add basic crud operations to potluckModel

---
+++ data/models/potlucksModel.js
@@ -0,0 +1,38 @@
+const db = require("../knexConfig.js");
+
+module.exports = {
+  getAll,
+  findById,
+  insert,
+  update,
+  remove
+};
+
+async function getAll() {
+  return await db("potlucks");
+}
+
+async function findById(id) {
+  return await db("potlucks")
+    .where({ id: Number(id) })
+    .first();
+}
+
+async function insert(potluck) {
+  const [id] = await db("potlucks").insert(potluck);
+  return db("potlucks")
+    .where({ id })
+    .first();
+}
+
+async function update(id, potluck) {
+  return await db("potlucks")
+    .where("id", Number(id))
+    .update(potluck);
+}
+
+async function remove(id) {
+  return await db("potlucks")
+    .where("id", Number(id))
+    .del();
+}
