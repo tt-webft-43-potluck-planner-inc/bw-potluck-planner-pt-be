f29d93e9a9889fa92cf726e3ee63d7a3ef8fbfef
Christopher Harrison <cnharrison@gmail.com>
2019-07-12 09:30:48 -0400

    switch back 😩

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,7 @@ module.exports = {
   },
 
   production: {
-    client: "mysql",
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migrations"
