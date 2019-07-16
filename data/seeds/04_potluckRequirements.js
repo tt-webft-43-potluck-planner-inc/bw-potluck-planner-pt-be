fed36c3f07ad51cbcb030d631fff3395520beae8
Christopher Harrison <cnharrison@gmail.com>
2019-07-16 12:20:00 -0400

    add POST /req/add skeleton, update migration

--- data/seeds/04_potluckRequirements.js
+++ data/seeds/04_potluckRequirements.js
@@ -5,7 +5,7 @@ exports.seed = async function(knex) {
   for (let i = 0; i < 100; i++) {
     let newRequirement = {
       foodType: faker.commerce.productAdjective(),
-      potluckRequirementsId: (Math.random() * (54 - 1 + 1)) << 0,
+      potluckId: (Math.random() * (54 - 1 + 1)) << 0,
       servings: (Math.random() * (10 - 1 + 1)) << 0
     };
     requirements.push(newRequirement);
